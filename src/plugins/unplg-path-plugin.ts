8c9bca02c62956007df3d84998df68b2a440852c
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-17 18:16:24 -0400

    refactor: Create new fetch plugin

--- src/plugins/unplg-path-plugin.ts
+++ src/plugins/unplg-path-plugin.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 import localforage from 'localforage';
 
 const fileCache = localforage.createInstance({
@@ -10,59 +10,26 @@ export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
+      //Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
 
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
+      //Handle rel paths in module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      //Handle main file of module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         };
-      });
-
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import react from 'react-dom';
-              console.log(react);
-            `,
-          };
-        }
-
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
-        };
-
-        await fileCache.setItem(args.path, result);
-
-        return result;
       });
     },
   };
