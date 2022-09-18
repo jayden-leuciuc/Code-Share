66dcc06448b9ba814b0d725b5bd1f43518c3c9ed
Jayden Leuciuc <68298467+jayden-leuciuc@users.noreply.github.com>
2022-09-18 11:59:18 -0400

    feat: Add caching to unpkg requests (#1)
    * Initial file delete
    
    * feat: Add transpiling functionality
    
    * feat: Add dynamic loading of npm modules
    
    * chore: Add localforage for caching
    
    * feat: Add base caching logic
    
    * style: Run formatter
    
    * refactor: Create new fetch plugin
    
    * feat: Add build support for css files
    
    * refactor(fetch): Reduce duplicated code with cache checking
    
    * refactor: Get esbuild.wasm file directly from unpkg.com

---
+++ src/plugins/unplg-path-plugin.ts
@@ -0,0 +1,36 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache',
+});
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      //Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      //Handle rel paths in module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      //Handle main file of module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
