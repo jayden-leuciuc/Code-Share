925e3c581945dfa384b754ae3cba6048fa545daa
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-18 13:24:18 -0400

    fix: Remove warnings

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -73,8 +72,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts"></iframe>
+      <iframe
+        title="preview"
+        ref={iframe}
+        srcDoc={html}
+        sandbox="allow-scripts"
+      ></iframe>
     </div>
   );
 };
--- src/plugins/unplg-path-plugin.ts
+++ src/plugins/unplg-path-plugin.ts
@@ -1,10 +1,4 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localforage from 'localforage';
-
-const fileCache = localforage.createInstance({
-  name: 'filecache',
-});
 
 export const unpkgPathPlugin = () => {
   return {
