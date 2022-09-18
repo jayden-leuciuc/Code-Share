f381b253b73e1d7f7fe3185028194e826339df38
Jayden Leuciuc <68298467+jayden-leuciuc@users.noreply.github.com>
2022-09-18 14:26:36 -0400

    feat: Add monaco editor and styling (#3)
    * chore: Add monaco editor dependency
    
    * chore: Add monacoEditor dependency
    
    * feat: Create bare CodeEditor component
    
    * feat: Add types and update options of CodeEditor component
    
    * chore: Add prettier dependency
    
    * feat: Add prettier formatting functionality
    
    * feat: Add styling to code editor
    
    * chore: Add bulmaswatch css depedency, downgrade react-scripts to v4
    
    * chore: Update package-lock

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unplg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -65,6 +67,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue='const a' onChange={(value)=> setInput(value)}/>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
