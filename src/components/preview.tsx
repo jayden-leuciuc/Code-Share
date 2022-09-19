c1f9ec76687ad8c4f8f79e82f298c8e330649e20
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-19 00:28:52 -0400

    feat: Add logic for handling async errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -12,14 +12,24 @@ const html = `
     <body>
       <div id="root"></div>
       <script>
-        window.addEventListener('message', (event)=> {
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = 
+          '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+        throw(err);
+        console.error(err);
+      };
+
+      window.addEventListener('error', (event) => {
+        event.preventDefault();
+        handleError(event);
+      });
+
+      window.addEventListener('message', (event) => {
           try{
             eval(event.data);
           } catch (err){
-            const root = document.querySelector('#root');
-            root.innerHTML = 
-              '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            throw(err);
+            handleError(err);
           }
           
         })
