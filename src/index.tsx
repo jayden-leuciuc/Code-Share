d2860618b9c608aa04cfb0ac9369511dc17b884e
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-18 23:07:33 -0400

    refactor: Move bundling logic into respective file

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,31 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import * as esbuild from 'esbuild-wasm';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unplg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
-  const [code, setCode] = useState('')
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text)
-
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  
 
   return (
     <div>
-      <CodeEditor initialValue='const a' onChange={(value)=> setInput(value)}/>
+      <CodeEditor
+        initialValue="console.log('Test');"
+        onChange={(value) => setInput(value)}
+      />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <Preview code={code}/>
+      <Preview code={code} />
     </div>
   );
 };
