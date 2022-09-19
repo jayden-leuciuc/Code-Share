3b72557e5a2c5cf6e2591755158d93db734e2435
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-19 15:20:19 -0400

    feat: Add logic for store

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { createRoot } from 'react-dom/client';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+
+import { Provider } from 'react-redux';
+import { store } from './redux';
+
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <TextEditor />
-    </>
+    </Provider>
   );
 };
 
