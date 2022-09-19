393873a410c457f414dd2c7162d0b78885e3ca46
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-09-18 23:09:47 -0400

    fix: Remove unused vars, update folder structure

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,6 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import { fetchPlugin } from '../plugins/fetch-plugin';
-import { unpkgPathPlugin } from '../plugins/unplg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unplg-path-plugin';
 
 let service: esbuild.Service;
 
